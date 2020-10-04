10819f5fa37fef2d090a92ba444877acfd390741
Hanna Szermer <hanna@parasoft.com>
2020-10-04 17:48:37 +0200

    Examples added

---
+++ BankExample/Parasoft.Dottest.Examples.Bank.Tests.MSTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Parasoft.Dottest.Examples.Bank.Tests.MSTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Parasoft.Dottest.Examples.Bank.Tests.MSTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("00711f1f-52ab-43e6-abd7-53d4bf9bc0e1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
